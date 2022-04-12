a6d824576a2da425bc69e556fa291973f954bd46
Hashimoto Taiga <2002421taiga@gmail.com>
2022-04-12 17:28:13 +0900

    3.13.17

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :end_users,skip: [:passwords], controllers: {
+    registrations: "end_user/registrations",
+    sessions: 'end_user/sessions'
+  }
+  devise_for :admins, skip: [:registrations, :passwords] , controllers: {
+    sessions: "admin/sessions"
+  }
+  namespace :end_user do
+    root 'homes#top'
+  end
 end
+
