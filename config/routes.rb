44b2280ddf8868103b17da49b35e5174898b1ef5
Hashimoto Taiga <2002421taiga@gmail.com>
2022-04-16 20:21:30 +0900

    3.16.20

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :passwords] , controllers: {
     sessions: "admin/sessions"
   }
-  namespace :end_user do
+  scope module: :end_user do
     root 'homes#top'
   end
 end
-
